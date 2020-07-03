bd572dfea38a55a8f210ff70cc95e6a83e9df7ba
nressa <mressanova@gmail.com>
2020-07-03 21:18:13 +0800

    Cascade auth pages

---
+++ resources/views/landing.blade.php
@@ -0,0 +1,27 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <h3 class="text-center pt-5 font-weight-bold">E-library</h3>
+            <p class="text-center">Your online library!</p>
+        </div>
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-header">Keep Track of your books</div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-header">Leave Comments and share to others</div>
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-header">Record your reading achievement</div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
