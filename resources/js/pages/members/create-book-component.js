75835dbf8ae4f95e68736cbe8ae1b39364b3db3d
nressa <mressanova@gmail.com>
2020-07-04 06:32:22 +0800

    Add model for the fields

--- resources/js/pages/members/create-book-component.js
+++ resources/js/pages/members/create-book-component.js
@@ -6,14 +6,14 @@ Vue.component('create-book-component', {
                 <div class="col-md-6">
                     <h3 class="text-center pt-5 pb-2 font-weight-bold">Add To List</h3>
                     <hr class="pb-4"/>
-                    <form method="post">
+                    <form method="post" @submit="submitForm">
                         <div class="form-group">
                             <label for="title" class="font-weight-bold">Title</label>
-                            <input name="title" id="title" class="form-control" type="text" />
+                            <input name="title" id="title" class="form-control" type="text" v-model="title" />
                         </div>
                         <div class="form-group  form-row">
                             <label class="font-weight-bold col-md-12">Author</label>
-                            <input name="author[]" class="form-control col-md-10" type="text" />
+                            <input name="author" class="form-control col-md-10" type="text" />
                             <div class="col-md-2">
                                 <button class="btn btn-primary btn-md rounded-circle font-weight-bold" v-on:click="addAuthor">+</button>
                             </div>
@@ -22,19 +22,19 @@ Vue.component('create-book-component', {
                         </div>
                         <div class="form-group">
                             <label for="date_published" class="font-weight-bold">Date Published</label>
-                            <input name="date_published" id="date_published" class="form-control" type="date" />
+                            <input v-model="date_published" id="date_published" class="form-control" type="date" />
                         </div>
                         <div class="form-group">
                             <label for="genres" class="font-weight-bold">Genre</label>
                             
-                            <select name="genres" id="genres" class="form-control" multiple>
+                            <select v-model="selected_genres" id="genres" class="form-control" multiple>
                                 <option disabled selected>--Select Options--</option>
                                 <option v-for="(option, i) in filterGenre" :index="i" :value="option.id">{{ option.name }}</option>
                             </select>
                         </div>
                         <div class="form-group">
                             <label for="description" class="font-weight-bold">Description</label>
-                            <textarea name="description" id="description" class="form-control" rows="5"></textarea>
+                            <textarea v-model="description" id="description" class="form-control" rows="5"></textarea>
                         </div>
 
                         <button type="submit" class="btn btn-primary btn-md btn-block">Save</button>
@@ -43,6 +43,16 @@ Vue.component('create-book-component', {
             </div>
         </div>
     `,
+    data() {
+        return {
+            title: null,
+            authors: [],
+            date_published: null,
+            selected_genres: [],
+            description: null,
+            data: []
+        }
+    },
     mixins: [GenreMixin],
     mounted() {
         this.fetchGenres()
@@ -54,10 +64,29 @@ Vue.component('create-book-component', {
             
             var new_row = document.createElement("input");
             new_row.setAttribute("class", "form-control mt-2");
-            new_row.setAttribute("name", "author[]");
+            new_row.setAttribute("name", "author");
 
             var element = document.getElementById("groupAuthor");
             element.appendChild(new_row)
+        },
+        submitForm(e)
+        {
+
+            e.preventDefault()
+            var inputfields = document.getElementsByName("author");
+            var ar_inputflds = inputfields.length;
+            for (var i = 0; i < ar_inputflds; i++) {
+                this.authors.push(inputfields[i].value);
+            }
+            this.data.push([
+                this.title,
+                this.authors,
+                this.date_published,
+                this.selected_genres,
+                this.description
+            ])
+
+            console.log(this.data)
         }
     },
     computed: {
